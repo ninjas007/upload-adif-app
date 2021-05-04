5d7a1dfcfc6a7e0c1d1b8a449cf6d0a26248cea2
Tilis Tiadi <39684743+ninjas007@users.noreply.github.com>
2021-05-04 14:14:55 +0700

    Update CronJob where member premium and not lifetime

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -9,56 +9,56 @@ class CronJobController extends Controller
 {
     public function index()
     {
-    	$users = User::all();
-
+    	$users = User::where('life_time', 0)->where('category', 'premium')->get();
+        
+        $return['users'] = [];
     	foreach ($users as $user) {
-            if ($user->life_time == 0) {
-                 $data = [
+    	    $data = [
                     'nama' => $user->name,
                     'kategori' => $user->category,
                     'callsign' => $user->callsign
                 ];
 
-                $date = date('Y-m-d', strtotime('+335 day', strtotime($user->register)));                
-                
-                if ($date == date('Y-m-d')) {
-                    
-                    $data['expired'] = false;
-
-                    \Mail::to($user->email)->send(new \App\Mail\ExpiredMail($data));
-                    $return['users'][] = [
-                        'name' => $user->name,
-                        'category' => $user->category,
-                        'callsign' => $user->callsign,
-                        'no_hp' => $user->no_hp
-                    ];
-                }
-
-                $date2 = date('Y-m-d', strtotime('+366 day', strtotime($user->register)));
-
-                if ($date2 == date('Y-m-d')) {
-                    
-                    $data['expired'] = true;
-
-                    // update inactive user
-                    $u = User::findOrFail($user->id);
-                    $u->active = 0;
-                    $u->updated_at = date('Y-m-d H:i:s');
-                    $u->save();
-
-                    \Mail::to($user->email)->send(new \App\Mail\ExpiredMail($data));
-                    $return['users'][] = [
-                        'name' => $user->name,
-                        'category' => $user->category,
-                        'callsign' => $user->callsign,
-                        'no_hp' => $user->no_hp
-                    ];
-                }
-            }
+            $date = date('Y-m-d', strtotime('+335 day', strtotime($user->register)));                
             
-        	$return['count'] = count($return['users']);
+            if ($date == date('Y-m-d')) {
+                
+                $data['expired'] = false;
 
-        	\Mail::to(User::find(1)->first()->email)->send(new \App\Mail\ExpiredAdminNotifMail($return));
+                \Mail::to($user->email)->send(new \App\Mail\ExpiredMail($data));
+                $return['users'][] = [
+                    'name' => $user->name,
+                    'category' => $user->category,
+                    'callsign' => $user->callsign,
+                    'no_hp' => $user->no_hp
+                ];
+            }
+
+            $date2 = date('Y-m-d', strtotime('+366 day', strtotime($user->register)));
+
+            if ($date2 == date('Y-m-d')) {
+                
+                $data['expired'] = true;
+
+                // update inactive user
+                $u = User::findOrFail($user->id);
+                $u->active = 0;
+                $u->updated_at = date('Y-m-d H:i:s');
+                $u->save();
+
+                \Mail::to($user->email)->send(new \App\Mail\ExpiredMail($data));
+                $return['users'][] = [
+                    'name' => $user->name,
+                    'category' => $user->category,
+                    'callsign' => $user->callsign,
+                    'no_hp' => $user->no_hp
+                ];
+            }
+    	}
+
+    	$return['count'] = count($return['users']);
+    	if($return['count'] > 0) {
+    	    \Mail::to(User::find(1)->first()->email)->send(new \App\Mail\ExpiredAdminNotifMail($return));
     	}
 
     }
