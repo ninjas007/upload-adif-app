d8591f66bc3e19ac9fd4fefbe31181106f616b60
adi <tiliztiadi@gmail.com>
2022-04-15 16:39:00 +0800

    ovveride login authenticated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return Response
+     */
+    public function authenticated(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'active' => 1])) {
+            return redirect()->route('home');
+        }
     }
 }
